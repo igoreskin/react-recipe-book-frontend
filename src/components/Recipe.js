1424005159dc8a7f57e6d45ca2d637953d00ec08
Igor Eskin <github email address>
2018-05-15 17:23:00 -0400

    Delete button added

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Recipe = ({recipe}) => {
   return (
     <div className="tile">
+      <span className="deleteButton">X</span>
       <h4>{recipe.title}</h4>
       <p>{recipe.ingredients}</p>
     </div>
--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Recipe from './Recipe';
 
 const RecipesList = (props) => {
-  const sortedRecipes = props.recipes.sort((a, b) => a.created_at < b.created_at ? 1 : -1);
+  const sortedRecipes = props.recipes.sort((a, b) => a.updated_at < b.updated_at ? 1 : -1);
   console.log(sortedRecipes);
   const renderRecipes = sortedRecipes.map((recipe) =>
     <Recipe recipe={recipe} key={recipe.id} />
